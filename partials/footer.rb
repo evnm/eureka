d2f0f9115e1b666085d2d4e4ae8a67259c545123
Evan Meagher <evan.meagher@gmail.com>
2009-08-05 20:08:55 -0700

    added h2 element to generate.rhtml, removed copyright from footer.rb, added style for h2

--- partials/footer.rb
+++ partials/footer.rb
@@ -1,3 +1,3 @@
 puts "<footer>"
-puts "        <p>Copyright &copy; 2009 <a href='http://evanmeagher.net/'>Evan Meagher</a></p>"
+puts "        <p>Built by <a href='http://evanmeagher.net/'>Evan Meagher</a> in the fine year of 2009</p>"
 puts "      </footer>"
