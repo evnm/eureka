2b20f464252d74beae49f1f9540a72f7b9535172
Evan Meagher <evan.meagher@gmail.com>
2009-08-06 18:48:51 -0700

    Modified about page and footer copy

--- partials/footer.rb
+++ partials/footer.rb
@@ -1,3 +1,3 @@
 puts "<footer>"
-puts "        <p>Built by <a href='http://evanmeagher.net/'>Evan Meagher</a> in the fine year of 2009</p>"
+puts "        <p>Built by <a href='http://evanmeagher.net/'>Evan Meagher</a></p>"
 puts "      </footer>"
