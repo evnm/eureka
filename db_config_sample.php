7417cdf60949328095ce98be3157154c369ead5d
Evan Meagher <evan.meagher@gmail.com>
2009-08-10 17:47:22 -0700

    Added a bit more documentation.

--- db_config_sample.php
+++ db_config_sample.php
@@ -1,6 +1,8 @@
 <?php
 // This file contains MySQL settings and returns a database handle
 // Insert the database name, username, and password below
+// Once complete, rename this file to db_config.php
+
 return mysql_connect('db_name', 'username', 'password')
         or die('Could not connect: ' . mysql_error());
 ?>
